9ee3635415d7dbc305d4333681222f77bc949d4a
nehashedge30 <nehashedge3012@gmail.com>
2024-12-17 00:41:54 +0530

    msg

--- src/HomePage.js
+++ src/HomePage.js
@@ -5,9 +5,11 @@ import './HomePage.css'; // Ensure you have the corresponding CSS
 import mumbaiImage from './images/mumbaiImage.jpg' ;
 import satara from './images/kaas.jpg';
 import goaImage from './images/goa.jpg';
+import logoImage from './images/logo.jpg';
+
 
 function HomePage() {
-  const [heroText, setHeroText] = useState('Welcome to MH Trip Planner'); // Dynamic hero text
+  const [heroText, setHeroText] = useState('Welcome to Easy Trip Planner'); // Dynamic hero text
 
   // Sample destinations data (can be fetched from an API or defined here)
   const destinations = [
@@ -27,13 +29,14 @@ function HomePage() {
       {/* Header Section */}
       <header className="navbar">
         <div className="logo">
-          <img src="./images/logo.jpg" alt="Logo" className="logo-image" />
-          MH Trip Planner
+        <img src={logoImage} className="logo-image" alt="Trip Planner Logo" />
+          Easy Trip Planner
         </div>
         <nav>
           <ul>
-            <li><Link to="/explore">Explore</Link></li>
-            <li><Link to="/login">Login</Link></li>
+            <li><Link to="/aboutus">About Us</Link></li>
+            {/* <li><Link to="/login">Login</Link></li> */}
+            <li><Link to="/feedback">Feedback</Link></li>
             <li><a href="#logout">Logout</a></li>
             <li><a href="/contact">Contact Us</a></li>
           </ul>
@@ -48,8 +51,12 @@ function HomePage() {
         <Link to="/explore" style={{ textDecoration: 'none', color: 'inherit' }}>
           Start Exploring
         </Link>
-        
         </button>
+        <button className="login-button">
+      <Link to="/login" style={{ textDecoration: 'none', color: 'inherit' }}>
+        Login
+      </Link>
+    </button>
       </main>
 
       {/* Featured Destinations Section - Dynamic Rendering */}
